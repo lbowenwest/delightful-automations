bfb39d6557c410d2b963a2ab94ce0a69d74eaddd
lygon <l.bowenwest@googlemail.com>
2023-11-06 17:35:35 +0000

    feat: add helpful crafting recipes

--- kubejs/server_scripts/example.js
+++ kubejs/server_scripts/example.js
@@ -4,3 +4,19 @@
 
 console.info('Hello, World! (Loaded server scripts)')
 
+ServerEvents.recipes(event => {
+
+    event.shaped("minecraft:saddle", ["LLL", "S S", "I I"], { L: "minecraft:leather", S: "minecraft:string", I: "minecraft:iron_nugget" })
+
+    // event.remove({ output: "supplementaries:rope" })
+
+
+    event.remove({ output: "backpacked:backpack" })
+    event.shaped(
+        "backpacked:backpack",
+        ["LLL", "SIS", "LLL"],
+        { I: "minecraft:iron_ingot", S: "minecraft:string", L: "minecraft:leather" }
+    )
+
+    event.shaped("minecraft:bundle", ["SLS", "L L", "LLL"], {S: "minecraft:string", L: "minecraft:leather"})
+})
