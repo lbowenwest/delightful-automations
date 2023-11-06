4beb2df36cf6dd0b075983c42ebcb920f01596b6
lygon <l.bowenwest@googlemail.com>
2023-11-06 20:15:13 +0000

    feat: add quiver recipe

--- kubejs/server_scripts/example.js
+++ kubejs/server_scripts/example.js
@@ -2,8 +2,6 @@
 
 // Visit the wiki for more info - https://kubejs.com/
 
-console.info('Hello, World! (Loaded server scripts)')
-
 ServerEvents.recipes(event => {
 
     event.shaped("minecraft:saddle", ["LLL", "S S", "I I"], { L: "minecraft:leather", S: "minecraft:string", I: "minecraft:iron_nugget" })
@@ -18,5 +16,17 @@ ServerEvents.recipes(event => {
         { I: "minecraft:iron_ingot", S: "minecraft:string", L: "minecraft:leather" }
     )
 
-    event.shaped("minecraft:bundle", ["SLS", "L L", "LLL"], {S: "minecraft:string", L: "minecraft:leather"})
+    event.shaped("minecraft:bundle", ["SLS", "L L", "LLL"], { S: "minecraft:string", L: "minecraft:leather" })
+
+    event.shaped("railways:benchcart", ["IWI", "III"], { I: "minecraft:iron_ingot", W: "minecraft:crafting_table" }).id("kubejs:craft_cart")
+    event.shaped("railways:jukeboxcart", ["IJI", "III"], { I: "minecraft:iron_ingot", J: "minecraft:jukebox" }).id("kubejs:juke_cart")
+
+    event.shaped('supplementaries:quiver', [
+        'L L',
+        'LSL',
+        ' L '
+    ], {
+        S: '#forge:string',
+        L: '#forge:leather'
+    })
 })
